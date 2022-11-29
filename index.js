5f0fb212f3e6f1d2ab5a5b6f4d17ca50c61d364f
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2022-11-29 15:33:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+    
+    // const found = array1.find(element => element > 10); -- syntaX
+
+    let result = record.find(record => record.result === "W");
+
+    // const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+    const sadReality=result ? result.year: undefined;
+    return sadReality;
+}
+superbowlWin()
